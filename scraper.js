091391c2cf6a0fff591e2eb34101f57cfab47b22
davidwole <davidwole20@gmail.com>
2024-11-25 09:26:30 +0100

    Adjusted the time and reduced bloat

--- scraper.js
+++ scraper.js
@@ -61,12 +61,7 @@ async function scrape() {
       craigslistLinks.map((link) => processLink(link))
     );
 
-    const testTimeFilter = new Date(Date.now() - 60 * 60 * 1000);
-    const timeFilter = new Date(Date.now() - 60 * 60 * 1000);
-
-    const testRecentListings = allListings
-      .flat()
-      .filter((item) => new Date(item.posted) > testTimeFilter);
+    const timeFilter = new Date(Date.now() - 20 * 60 * 1000);
 
     const recentListings = allListings
       .flat()
